04fd638eabf7d9d205b4799d4c36d1c09341968d
joaopamarall <joaoamaral56@hotmail.com>
2023-12-22 21:44:34 -0300

    refactor: refatorar códigos com uma estruturação mais clara

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,114 +1,128 @@
 <template>
-  <div class="login">
-    <!--<img class="logo" />-->
-    <div class="login-card">
-      <h1><strong>Entre na sua conta</strong></h1>
-      <div>Para acessar sua conta informe seu usuário e senha</div>
-      <div class="login-card-p1">Username</div>
-      <input type="text" placeholder="Seu Username" v-model="username" />
-      <div class="login-card-p2">Senha</div>
-      <input type="password" placeholder="Sua Senha" v-model="senha" />
-      <div class="login-card-p3">Esqueci minha senha</div>
-      <button class="login-card-button" @click="fazerLogin(username, senha)">FAZER LOGIN</button>
-    </div>
-    <div class="direcionar-cadastro">
-        <div class="login-card-p4">Ainda não tem conta?</div><router-link to="/PlanosView" class="direcionar-cadastro-link"> Cadastre-se</router-link>
-    </div>
-  </div>
+	<div class="login">
+		<!--<img class="logo" />-->
+		<div class="login-card">
+			<h1><strong>Entre na sua conta</strong></h1>
+			<div>Para acessar sua conta informe seu usuário e senha</div>
+			<div class="login-card-p1">Username</div>
+			<input type="text" v-model="username" />
+			<div class="login-card-p2">Senha</div>
+			<input type="password" v-model="senha" />
+			<div class="login-card-p3" @click="forgotPasswordAlert">
+				Esqueci minha senha
+			</div>
+			<button class="login-card-button" @click="fazerLogin(username, senha)">
+				FAZER LOGIN
+			</button>
+		</div>
+		<div class="direcionar-cadastro">
+			<div class="login-card-p4">Ainda não tem conta?</div>
+			<router-link to="/PlanosView" class="direcionar-cadastro-link">
+				Cadastre-se</router-link
+			>
+		</div>
+	</div>
 </template>
 <script>
 import axios from "axios";
 export default {
-  name: "LoginView",
-  data() {
-    return {
-      username: "",
-      senha: "",
-    };
-  },
-  methods: {
-    fazerLogin(username, senha) {
-      axios
-        .post("https://fakestoreapi.com/auth/login", {
-          username: username,
-          password: senha,
-        })
-        .then((res) => {
-          console.log("Esta é uma resposta da API", res.data);
-          this.$router.push("/LogadoView");
-        })
-        .catch((error) => {
-          console.log("Não consegui fazer login, deu agum erro na API", error);
-        });
-    },
-  },
+	name: "LoginView",
+	data() {
+		return {
+			username: "",
+			senha: "",
+		};
+	},
+	methods: {
+		fazerLogin(username, senha) {
+			axios
+				.post("https://fakestoreapi.com/auth/login", {
+					username: username,
+					password: senha,
+				})
+				.then((res) => {
+					console.log("Esta é uma resposta da API", res.data);
+					this.$router.push("/LogadoView");
+				})
+				.catch((error) => {
+					console.log("Não consegui fazer login, deu agum erro na API", error);
+				});
+		},
+		forgotPasswordAlert() {
+			alert("Solicitação de nova senha enviada com sucesso!");
+		},
+	},
 };
 </script>
 <style>
 body {
-  margin: 0;
-  background-color: #FAFAFC;
-  color:#292D32;
-  font-family: 'Montserrat', sans-serif;
-  font-family: 'Sora', sans-serif;
+	margin: 0;
+	background-color: #fafafc;
+	color: #292d32;
+	font-family: "Montserrat", sans-serif;
+	font-family: "Sora", sans-serif;
 }
 .login {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  height: 100vh;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	height: 100vh;
 }
 .login-card {
-  background-color: #ffffff;
-  height: 470px;
-  width: 580px;
-  padding: 30px;
-  border: 1px solid #E6EAF2;
-  border-radius: 4px;
-  display: flex;
-  flex-direction: column;
-  justify-content:space-between;
+	background-color: #ffffff;
+	height: 470px;
+	width: 580px;
+	padding: 30px;
+	border: 1px solid #e6eaf2;
+	border-radius: 4px;
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
 }
 .login-card-p3 {
-  padding: 0 0 24px 0;
-  text-align: end;
-  color:#515D74;
+	padding: 0 0 24px 0;
+	text-align: end;
+	color: #515d74;
+	cursor: pointer;
 }
 .login-card-button {
-  background-color: #f30168;
-  color: #ffffff;
-  height: 80px;
-  border: 1px solid #f30168;
-  border-radius: 4px;
-  padding: 0;
-  cursor: pointer;
+	background-color: #f30168;
+	color: #ffffff;
+	height: 80px;
+	font-size: 16px;
+	border: none;
+	border-radius: 8px;
+	cursor: pointer;
 }
 .direcionar-cadastro {
-  display: flex;
-  align-items: center;
-  gap: 10px;
+	margin-top: 40px;
+	display: flex;
+	align-items: center;
+	gap: 10px;
 }
 .direcionar-cadastro-link {
-  color: #f30168;
+	color: #f30168;
 }
 h1 {
-  margin: 0;
+	margin: 0;
 }
 h4 {
-  margin: 0;
-  padding: 10px 0 0 0;
+	margin: 0;
+	padding: 10px 0 0 0;
 }
 div {
-  margin: 0;
+	margin: 0;
 }
-.login-card-p1, .login-card-p2 {
-  padding: 28px 0 0 0;
+.login-card-p1,
+.login-card-p2 {
+	padding: 28px 0 0 0;
 }
 input {
-  height:80px;
-  border: 1px solid #515D7480;
-  border-radius: 4px;
-  font-size: medium;
+	height: 80px;
+	border: 1px solid #515d7480;
+	border-radius: 8px;
+	font-size: 28px;
+	padding: 0 20px;
 }
 </style>
--- src/views/PlanosView.vue
+++ src/views/PlanosView.vue
@@ -1,58 +1,58 @@
 <template>
-    <div class="planos">
-        <planos-title-component-vue />
-        <div class="botao-destaque">
-            MAIS USADO
-        </div>
-        <div class="opcoes-planos">
-            <planos-component-vue tipo="Hospedagem 1" preco="0"/>
-            <planos-component-vue tipo="Hospedagem 2" preco="499" condicao="Cobrado mensalmente" siteMaisVisita="Ideal para site com mais de 30k de visitas." paisServidor="Brasil" />
-            <planos-component-vue tipo="Hospedagem 3" preco="999" condicao="Cobrado mensalmente" siteMaisVisita="Ideal para site com mais de 60k de visitas." paisServidor="Brasil" />
-        </div>
-    </div>
+	<div class="planos">
+		<planos-title-component-vue />
+		<div class="botao-destaque">MAIS USADO</div>
+		<div class="opcoes-planos">
+			<planos-component-vue tipo="Hospedagem 1" preco="0" />
+			<planos-component-vue
+				tipo="Hospedagem 2"
+				preco="499"
+				condicao="Cobrado mensalmente"
+				siteMaisVisita="Ideal para site com mais de 30k de visitas."
+				paisServidor="Brasil"
+			/>
+			<planos-component-vue
+				tipo="Hospedagem 3"
+				preco="999"
+				condicao="Cobrado mensalmente"
+				siteMaisVisita="Ideal para site com mais de 60k de visitas."
+				paisServidor="Brasil"
+			/>
+		</div>
+	</div>
 </template>
 <script>
-import PlanosTitleComponentVue from '@/components/planos/PlanosTitleComponent.vue';
-import PlanosComponentVue from '@/components/planos/PlanosComponent.vue'
+import PlanosTitleComponentVue from "@/components/planos/PlanosTitleComponent.vue";
+import PlanosComponentVue from "@/components/planos/PlanosComponent.vue";
 
 export default {
-    name:'planos-view',
-    components: {
-        PlanosComponentVue,
-        PlanosTitleComponentVue
-    },
-    data () {
-        return {
-            
-        }
-    }
-}
+	components: {
+		PlanosComponentVue,
+		PlanosTitleComponentVue,
+	},
+};
 </script>
 
 <style>
 body {
-    background-color: aliceblue;
+	background-color: aliceblue;
 }
 .planos {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
 }
 .botao-destaque {
-    background-color: rgb(32, 189, 27);
-    color: #ffffff;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    border-radius: 8px;
-    padding:2px;
-    height: 32px;
-    width: 120px;
+	background-color: rgb(32, 189, 27);
+	color: #ffffff;
+	border-radius: 8px;
+	padding: 8px;
+	position: absolute;
+	top: 224px;
 }
 .opcoes-planos {
-    display: flex;
-    justify-content: center;
-    gap: 26px;
+	display: flex;
+	gap: 26px;
 }
 </style>
