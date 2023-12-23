c1e5bcd2721f113f72787ac3d3048d780d6debfb
joaopamarall <joaoamaral56@hotmail.com>
2023-12-22 21:42:43 -0300

    feat: criar componente LogadoView.vue

--- src/views/LogadoView.vue
+++ src/views/LogadoView.vue
@@ -1,15 +1,43 @@
 <template>
-    <div>
-        <h1>{{ aviso }}</h1>
+    <div class="logado-view">
+        <div class="button-user">
+            <button type="submit">J</button>
+        </div>
+        <div class="body-user">
+            <h1>Olá João,</h1>
+            <div>Seja Bem-vindo a sua conta de hospedagem.</div>
+            <iframe width="800" height="500" src="https://www.youtube.com/embed/vqZHkdoyNNg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            <div>Caso tenha alguma dúvida, visite nossa central de ajuda.</div>
+        </div>
     </div>
 </template>
 <script>
 export default {
     name: 'LogadoView',
-    data() {
-        return {
-            aviso: 'VOCE ESTÁ LOGADO!!!'
-        }
-    }
+
 }
 </script>
+<style scoped>
+.logado-view {
+    display: flex;
+    flex-direction: column;
+}
+.button-user {
+    display: flex;
+    justify-content: flex-end;
+    margin: 16px;
+}
+.body-user {
+    display: flex;
+    flex-direction: column;
+    margin-left: 20rem;
+}
+button {
+    height: 52px;
+    width: 52px;
+    border: none;
+    border-radius: 40px;
+    background-color: #D9D9D9;
+}
+
+</style>
