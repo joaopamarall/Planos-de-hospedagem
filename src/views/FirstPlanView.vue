cc75fd95f8eb0ade073883ec61d0342d6e060e78
joaopamarall <joaoamaral56@hotmail.com>
2023-12-22 21:41:28 -0300

    feat: criar componente FirstPlan.vue

---
+++ src/views/FirstPlanView.vue
@@ -0,0 +1,25 @@
+<template>
+	<div class="firstPlan">
+		<planos-title-component-vue />
+		<div class="content">
+			<FormData />
+			<planos-component-vue tipo="Hospedagem 1" preco="0" />
+		</div>
+	</div>
+</template>
+
+<script>
+import PlanosTitleComponentVue from "@/components/planos/PlanosTitleComponent.vue";
+import PlanosComponentVue from "@/components/planos/PlanosComponent.vue";
+import FormData from "@/components/FormData.vue";
+export default {
+	components: { PlanosTitleComponentVue, PlanosComponentVue, FormData },
+};
+</script>
+<style>
+.content {
+	display: flex;
+	justify-content: center;
+	gap: 20px;
+}
+</style>
